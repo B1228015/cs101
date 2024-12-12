2ddf2bf3bdec1e891402ac72b6a9250923b0db70
B1228015 <161295861+B1228015@users.noreply.github.com>
2024-12-12 15:05:41 +0800

    Create main3.c

---
+++ EX4/main3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    for(int i=1;i<=9;i++)
+    {
+        printf("1*%d=%d ",i,1*i);
+        printf("2*%d=%d ",i,2*i);
+        printf("3*%d=%d ",i,3*i);
+        printf("4*%d=%d ",i,4*i);
+        printf("5*%d=%d ",i,5*i);
+        printf("6*%d=%d ",i,6*i);
+        printf("7*%d=%d ",i,7*i);
+        printf("8*%d=%d ",i,8*i);
+        printf("9*%d=%d ",i,9*i);
+        printf("\n");
+    }
+
+    return 0;
+}
